0c2c52331ca2cee8d071e3cea184fd61ebebd735
Neres <neresfjcomunic@gmail.com>
2022-09-07 15:54:53 -0300

    feat: add DataBase

--- backend/src/main/java/com/springreact/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/springreact/dsmeta/entities/Sale.java
@@ -1,9 +1,14 @@
 package com.springreact.dsmeta.entities;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "tb_sales")
 public class Sale {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String sellerName;
     private Integer visited;
