d5069f612cce32d6190a4b7316b2700a19863e2c
Neres <neresfjcomunic@gmail.com>
2022-09-07 11:47:42 -0300

    feat: add component Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import NotificationButton from "./components/NotificationButton"
-
+import Header from "./components/Header"
 function App() {
   return(
     <>
-      <h1>Olá, Mundo!</h1>
-      <NotificationButton/>
+      <Header />
+      <NotificationButton />
     </>
   )
 }
